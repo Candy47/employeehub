ba9b2b8749664efb52e667c2590ebcc057cbe548
Candy47 <candyroll580@gmail.com>
2026-08-30 15:59:11 +0530

    Made health api of auth service public

--- auth-service/src/main/java/com/employeehub/auth/config/SecurityConfig.java
+++ auth-service/src/main/java/com/employeehub/auth/config/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/api/v1/auth/register",
                                 "/api/v1/auth/login",
+                                "/api/v1/health",
                                 "/actuator/**"
                         ).permitAll()
                         .anyRequest().authenticated())
